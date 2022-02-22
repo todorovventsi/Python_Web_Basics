cd5647ec6c19ee1a284687274c21129e6245ba47
Ventsislav Todorov <todorov.ventsi@gmail.com>
2022-02-22 16:51:59 +0200

    added models for Profile and Notes

--- exam_prep_03/notes_app/notes_app/notes/models.py
+++ exam_prep_03/notes_app/notes_app/notes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    FIRSTNAME_MAX_LENGTH = 20
+    LASTNAME_MAX_LENGTH = 20
+
+    first_name = models.CharField(
+        max_length=FIRSTNAME_MAX_LENGTH,
+    )
+
+    last_name = models.CharField(
+        max_length=LASTNAME_MAX_LENGTH,
+    )
+
+    age = models.IntegerField()
+
+    image = models.URLField()
+
+
+class Note(models.Model):
+    TITLE_MAX_LENGTH = 30
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LENGTH,
+    )
+
+    image = models.URLField()
+
+    content = models.TextField()
